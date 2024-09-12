46deb91f8f00d1de135d3403d6d6ce012ecd8524
Hans Mbangu [Work Notebook] <hansmbangu@gmail.com>
2024-09-13 00:10:15 +0200

    build the new issue page

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,3 +1,12 @@
+import { Button } from "@radix-ui/themes";
+import Link from "next/link";
+
 export default function Issues() {
-  return <div>Issues</div>;
+  return (
+    <div>
+      <Button>
+        <Link href={"/issues/new"}>New Issue</Link>
+      </Button>
+    </div>
+  );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,3 @@
-import { Button } from "@radix-ui/themes";
-
 export default function Home() {
-  return (
-    <div>
-      <Button>Test Theme</Button>
-    </div>
-  );
+  return <div>DashBoard Page</div>;
 }
