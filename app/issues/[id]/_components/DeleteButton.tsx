2a42dddb990dcc7baa8e698e409f88605c4d2b6d
Hans Mbangu [Work Notebook] <hansmbangu@gmail.com>
2024-09-14 22:24:07 +0200

    add a delet confermation dialog box

--- app/issues/[id]/_components/DeleteButton.tsx
+++ app/issues/[id]/_components/DeleteButton.tsx
@@ -1,12 +1,35 @@
-import { Button } from "@radix-ui/themes";
-import Link from "next/link";
-import { FaPencil } from "react-icons/fa6";
+"use client";
+
+import { AlertDialog, Button, Flex } from "@radix-ui/themes";
+import { FaDeleteLeft } from "react-icons/fa6";
 
 export default function DeleteButton({ issueId }: { issueId: number }) {
   return (
-    <Button color="red">
-      <FaPencil />
-      <Link href={`/issues/${issueId}/edit`}>Delete Issue</Link>
-    </Button>
+    <AlertDialog.Root>
+      <AlertDialog.Trigger>
+        <Button color="red">
+          <FaDeleteLeft />
+          Delete Issue
+        </Button>
+      </AlertDialog.Trigger>
+
+      <AlertDialog.Content>
+        <AlertDialog.Title>Delete Issue?</AlertDialog.Title>
+        <AlertDialog.Description>
+          Are you sure you want to delete this issue. This Action canot be
+          undon.
+        </AlertDialog.Description>
+        <Flex mt={"4"} gap={"4"}>
+          <AlertDialog.Cancel>
+            <Button variant="soft" color="gray">
+              No
+            </Button>
+          </AlertDialog.Cancel>
+          <AlertDialog.Action>
+            <Button color="red">Yes</Button>
+          </AlertDialog.Action>
+        </Flex>
+      </AlertDialog.Content>
+    </AlertDialog.Root>
   );
 }
