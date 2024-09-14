770291e5edcea3a5b7106ee6e519ab4f6bbf41b8
Hans Mbangu [Work Notebook] <hansmbangu@gmail.com>
2024-09-14 19:07:07 +0200

    build api for updating issue

---
+++ app/api/issues/[id]/route.ts
@@ -0,0 +1,32 @@
+import { issueSchema } from "@/app/lib/validationSchema";
+import prisma from "@/prisma/client";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function PATCH(
+  req: NextRequest,
+  { params }: { params: { id: string } }
+) {
+  const body = await req.json();
+
+  const validation = issueSchema.safeParse(body);
+
+  const { success, data, error } = validation;
+  const { id } = params;
+
+  if (!success)
+    return NextResponse.json({ error: error.format() }, { status: 400 });
+
+  const issueToUpdate = await prisma.issue.findUnique({
+    where: { id: parseInt(id) },
+  });
+
+  if (!issueToUpdate)
+    return NextResponse.json({ error: "Invalid Issue." }, { status: 404 });
+
+  const updatedIssue = await prisma.issue.update({
+    where: { id: issueToUpdate.id },
+    data,
+  });
+
+  return NextResponse.json(updatedIssue);
+}
