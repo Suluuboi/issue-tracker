79b0138aa86712743d250f5106f34ca309133052
Hans Mbangu [Work Notebook] <hansmbangu@gmail.com>
2024-09-17 15:20:56 +0200

    implement front end paginations

--- app/components/Pagination.tsx
+++ app/components/Pagination.tsx
@@ -1,21 +1,26 @@
+"use client";
+
 import { Flex, Text } from "@radix-ui/themes";
 import Button from "./Button";
 import { FaAngleLeft, FaAngleRight } from "react-icons/fa6";
 import { FaAngleDoubleLeft, FaAngleDoubleRight } from "react-icons/fa";
+import { useRouter, useSearchParams } from "next/navigation";
 
 interface Props {
   itemCount: number;
   pageSize: number;
-  currentPage: number;
 }
 
-export default function Pagination({
-  currentPage,
-  itemCount,
-  pageSize,
-}: Props) {
+export default function Pagination({ itemCount, pageSize }: Props) {
+  const router = useRouter();
+  const searchParams = useSearchParams();
+  const pageParams = searchParams.get("page");
+
+  const currentPage = parseInt(pageParams?.toString() || "1");
+
   const pageCount = Math.ceil(itemCount / pageSize);
   if (pageCount <= 1) return null;
+
   return (
     <Flex align={"center"} justify={"center"}>
       <PageControls />
@@ -25,23 +30,40 @@ export default function Pagination({
   function PageControls() {
     return (
       <Flex justify={"center"} gap={"2"} align={"center"}>
-        <Button disabled={currentPage === 1}>
+        <Button disabled={currentPage === 1} onClick={() => changePage(1)}>
           <FaAngleDoubleLeft />
         </Button>
-        <Button variant="soft" disabled={currentPage === 1}>
+        <Button
+          variant="soft"
+          disabled={currentPage === 1}
+          onClick={() => changePage(currentPage - 1)}
+        >
           <FaAngleLeft />
         </Button>
 
         <Text size={"2"}>
           Page {currentPage} of {pageCount}
         </Text>
-        <Button variant="soft" disabled={currentPage === pageCount}>
+        <Button
+          variant="soft"
+          disabled={currentPage === pageCount}
+          onClick={() => changePage(currentPage + 1)}
+        >
           <FaAngleRight />
         </Button>
-        <Button disabled={currentPage === pageCount}>
+        <Button
+          disabled={currentPage === pageCount}
+          onClick={() => changePage(pageCount)}
+        >
           <FaAngleDoubleRight />
         </Button>
       </Flex>
     );
+  }
+
+  function changePage(page: number) {
+    const params = new URLSearchParams(searchParams);
+    params.set("page", page.toString());
+    router.push(`?${params.toString()}`);
   }
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
+"use client";
 import Pagination from "./components/Pagination";
 
 export default function Home() {
-  return <Pagination currentPage={2} itemCount={30} pageSize={10} />;
+  return <Pagination itemCount={30} pageSize={10} />;
 }
